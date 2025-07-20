d00a01858af8f16228ba3b208cec078568eb81f8
Zainab <tariqzainab1190@gmail.com>
2025-07-21 02:36:06 +0500

    Fixed fanta.css import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import Fanta from "./Fanta";
+import App from "./App.jsx";
+import "./Fanta.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
